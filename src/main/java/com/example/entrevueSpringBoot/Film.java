a00d6ebeae1d026dde1f72a00a9a33ee0be95476
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2022-11-26 14:50:56 -0500

    Justification de l'usage des entités ORM comme dtos spécifiquement dans cette application

--- src/main/java/com/example/entrevueSpringBoot/Film.java
+++ src/main/java/com/example/entrevueSpringBoot/Film.java
@@ -35,7 +35,20 @@ public class Film {
     },
             // there is only one endpoint in the app
             // and it returns a film & its acteurs
-            // eager is fine and will save round-trips to the database
+            // EAGER is fine and will save round-trips to the database
+            //
+            // the only endpoints in this app are managing Film and Acteur
+            // with the exact same fields as the entities so this app is using
+            // the ORM entities as dtos on the endpoints for simplicity and conciseness
+            // (avoiding dto classes and mappers that are useless for this very specific simple app)
+            //
+            // to prevent the View (JSON serialization process) modifying the underlying database,
+            // we set property "spring.jpa.open-in-view=false" ; EAGER is handy because it prevents a
+            // "org.hibernate.LazyInitializationException : could not initialize proxy – no Session"
+            // when jackson calls Film#getActeurs()
+            //
+            // if we used fetch = LAZY we would need to call #getActeurs beforehand (purposely or
+            // by mapping to a dto) in order to avoid the LazyInitializationException
             fetch = EAGER
     )
     // TODO: can an acteur only play in one film ?
